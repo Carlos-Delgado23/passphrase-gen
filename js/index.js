a8fbb734d2291f028cebc10476f73eb14fbe223f
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-02-13 11:20:50 -0600

    just need word db

--- js/index.js
+++ js/index.js
@@ -1,56 +1,81 @@
 import { wordList } from './word-db-list.js';
+"use strict";
+
+$(document).ready(function () {
+  alert('The DOM has finished loading!');
+
+  // VARIABLES //
+  const results = $('#result');
+  const UNIsym = [33, 47];
+  const UNInum = [48, 57];
+  const UNIupper = [65, 90];
+  const UNIlower = [97, 122];
+
+  // EVENT LISTENERS //
+  $('#generate').click(() => {
+    // DOM SETTINGS DECLARED //
+    let length = $('#length').val();
+    const upper = $('#uppercase')[0].checked;
+    const lower = $('#lowercase')[0].checked;
+    const numbers = $('#numbers')[0].checked;
+    const symbols = $('#symbols')[0].checked;
+
+    let randSelector = [];
+    let password = [];
+
+    if (upper == true) {
+      for (let i = UNIupper[0]; i < UNIupper[1]; i++) {
+        randSelector.push(i);
+      }
+    }
+    if (lower === true) {
+      for (let i = UNIlower[0]; i < UNIlower[1]; i++) {
+        randSelector.push(i);
+      }
+    }
+    if (numbers === true) {
+      for (let i = UNInum[0]; i < UNInum[1]; i++) {
+        randSelector.push(i);
+      }
+    }
+    if (symbols === true) {
+      for (let i = UNIsym[0]; i < UNIsym[1]; i++) {
+        randSelector.push(i);
+      }
+    }
+
+    for (let i = 0; i < length; i++) {
+      password.push(String.fromCharCode(randSelector[Math.floor(Math.random() * randSelector.length)]));
+    }
+
+    results[0].textContent = password.join('');
+  });
 
 
+  // COPY TO CLIPBOARD
+  var btn = document.getElementById('clipboard');
+  var clipboard = new ClipboardJS(btn);
 
-// GET ELEMENTS //
-const result = document.getElementById('result');
-const upper = document.getElementById('uppercase');
-const lower = document.getElementById('lowercase');
-const num = document.getElementById('numbers');
-const sym = document.getElementById('symbols');
+  // clipboard.on('success', function (e) {
+  //   console.log(e);
+  // });
 
-
-let pwGen = () => {
-  let resultOutput = [];
-
-  if (upper === 'checked') {
-
-  }
-  if (lower === 'checked') {
-
-  }
-  if (num === 'checked') {
-
-  }
-  if (sym === 'checked') {
-
-  }
-}
+  // clipboard.on('error', function (e) {
+  //   console.log(e);
+  // });
 
 
+  // GENERATE SIX WORDS FROM WORD DB //
+  let wordArrGen = (wordList) => {
+    let wordArr = [];
 
-
-
-
-
-
-
-
-
-
-
-// GENERATE SIX WORDS FROM WORD DB //
-let wordArrGen = (wordList) => {
-  let wordArr = [];
-
-  for (var i = 0; i < 6; i++) {
-    wordArr.push(
-      wordList[Math.round(Math.random() * wordList.length)]);
+    for (var i = 0; i < 6; i++) {
+      wordArr.push(
+        wordList[Math.round(Math.random() * wordList.length)]);
+    };
+    return wordArr;
   };
-  return wordArr;
-};
 
-console.log(wordArrGen(wordList));
+  console.log(wordArrGen(wordList));
 
-
-
+});
